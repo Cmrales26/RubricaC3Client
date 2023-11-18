98133f299f904e9c75a41f9dc7b663f9ac752d81
Nelson Morales <camilomorales2615@gmail.com>
2023-11-18 16:24:14 -0500

    Remove Host from Rundev

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import dns from 'dns'
-import react from '@vitejs/plugin-react';
-
-dns.setDefaultResultOrder('verbatim')
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 5173
-  }
 });
